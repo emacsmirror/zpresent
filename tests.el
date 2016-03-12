19adc05ecdc414ff2a6364c7b67c9380876fb094
Zachary Kanfer <zkanfer@gmail.com>
2016-03-12 00:57:03 -0500

    Add a few tests.

---
+++ tests.el
@@ -0,0 +1,61 @@
+;;; tests.el -- tests for zpresent.el
+
+;;; Commentary:
+
+;;; Code:
+
+
+;; (ert-deftest format-block-helper-single-block ()
+;;   (should (org-structure/hash-tables-equal #s(hash-table data (title "a title" body ("one" "two")))
+;;                                            (car (zpresent-format-block-helper (make-hash-table)
+;;                                                                               (make-hash-table 'title "a title"
+;;                                                                                                'body nil)
+;;                                                                               nil)))))
+
+(ert-deftest make-slide/title-only/check-title ()
+  (should (equal "I'm the title!"
+                 (gethash 'title (zpresent/make-slide "I'm the title!")))))
+
+(ert-deftest make-slide/title-only/no-body ()
+  (should-not (gethash 'body (zpresent/make-slide "I'm the title!"))))
+
+(ert-deftest make-slide/title-only/proper-things-added ()
+  (should (equal 2
+                 (hash-table-count (zpresent/make-slide "I'm the title!")))))
+
+(ert-deftest make-slide/body/no-body ()
+  (should (equal "I'm a body"
+                 (gethash 'body (zpresent/make-slide "I'm the title!" "I'm a body")))))
+
+(ert-deftest make-slide/body/no-body ()
+  (should (equal 2
+                 (hash-table-count (zpresent/make-slide "I'm the title!" "I'm a body")))))
+
+
+(ert-deftest extend-slide/original-slide-not-updated ()
+  (let* ((original-slide (zpresent/make-slide "I'm the title!"))
+         (new-slide (zpresent/extend-slide original-slide (car (org-structure "* New body text.")) 1)))
+    (should (equal 2 (hash-table-count original-slide)))
+    (should-not (gethash 'body original-slide))))
+
+(ert-deftest extend-slide/check-title ()
+  (let* ((original-slide (zpresent/make-slide "I'm the title!"))
+         (new-slide (zpresent/extend-slide original-slide (car (org-structure "* New body text.")) 1)))
+    (should (equal "I'm the title!"
+                   (gethash 'title new-slide)))))
+
+(ert-deftest extend-slide/check-new-body ()
+  (let* ((original-slide (zpresent/make-slide "I'm the title!"))
+         (new-slide (zpresent/extend-slide original-slide (car (org-structure "* New body text.")) 1)))
+    (should (equal (list " ▸ New body text.")
+                   (gethash 'body new-slide)))))
+
+(ert-deftest extend-slide/check-added-body ()
+  (let* ((original-slide (zpresent/make-slide "I'm the title!" "Initial body."))
+         (new-slide (zpresent/extend-slide original-slide (car (org-structure "* New body text.")) 1)))
+    (should (equal (list "Initial body." " ▸ New body text.")
+                   (gethash 'body new-slide)))))
+
+
+
+;;; tests.el ends here
