f59d3062f25acf03f15334aa6710ecd586e73ac8
Zachary Kanfer <zkanfer@gmail.com>
2017-01-27 00:38:17 -0500

    reorder header stuff

--- zpresent.el
+++ zpresent.el
@@ -1,7 +1,9 @@
 ;;; zpresent.el --- Simple presentation mode based on org files.  -*- lexical-binding: t; -*-
-;; Copyright 2015 Zachary Kanfer <zkanfer@gmail.com>
 
 ;; Version: 0.1
+;; This file is not part of GNU Emacs.
+
+;; Copyright 2015 Zachary Kanfer <zkanfer@gmail.com>
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -15,6 +17,7 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 ;; Homepage: https://bitbucket.org/zck/zpresent.el
 
