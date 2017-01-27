d475f048c4f47d805bac9186ef32b982262d09d8
Zachary Kanfer <zkanfer@gmail.com>
2017-01-27 00:38:51 -0500

    update copyright dates.

--- zpresent.el
+++ zpresent.el
@@ -3,7 +3,7 @@
 ;; Version: 0.1
 ;; This file is not part of GNU Emacs.
 
-;; Copyright 2015 Zachary Kanfer <zkanfer@gmail.com>
+;; Copyright 2015-2017 Zachary Kanfer <zkanfer@gmail.com>
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
