cc24d6abfc0828ae38192d79bdb6d1a9aab99ff5
Zachary Kanfer <zkanfer@gmail.com>
2017-06-09 01:11:46 -0400

    add a little bit of commentary.

--- zpresent.el
+++ zpresent.el
@@ -28,6 +28,12 @@
 
 ;;; Commentary:
 
+;; This package enables presenting org files inside Emacs in a form
+;; that's like "traditional" presentation software.
+
+;; A tutorial can be accessed by opening `tutorial.org`, then running
+;; `M-x zpresent`.
+
 ;;; TODOs:
 ;;zck why symbols here, but keywords in org-structure?
 
